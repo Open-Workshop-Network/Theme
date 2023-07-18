c9086653d4d2fa76e60f0a282f367da7d82d1623
Tom Lynch <me@unknowndomain.co.uk>
2023-07-18 21:24:41 +0100

    Fix change in wordpress variables.
    F

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 		<!-- <meta name="author" content="Name"> -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-		<link rel="shortcut icon" href="<?php echo THEME_DIR ?>/path/favicon.ico" />
+		<link rel="shortcut icon" href="<?php echo get_template_directory() ?>/path/favicon.ico" />
 		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ) ?> RSS2 Feed" href="<?php bloginfo( 'rss2_url' ) ?>" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ) ?>" />
 
@@ -17,13 +17,8 @@
 		<?php wp_head() ?>
 	</head>
 	<body <?php body_class() ?>>
-		<!--[if lt IE 8]>
-			<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-		<![endif]-->
-
 		<header>
 			<div class="logo">
 				<a href="<?php bloginfo( 'url' ) ?>" class="home">Home</a>
-				<!-- <span class="city"><?php bloginfo( 'description' ) ?><a href="http://openworkshopnetwork.com/" class="switch" title="Switch City">Switch City</a></span> -->
 			</div>
 		</header>
